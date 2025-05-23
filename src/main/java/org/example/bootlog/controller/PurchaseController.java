7bceb8bee6db07c14f1348156098f62be34596d6
qus0in <qus0in@gmail.com>
2025-05-23 10:50:30 +0900

    중간 푸시

--- src/main/java/org/example/bootlog/controller/PurchaseController.java
+++ src/main/java/org/example/bootlog/controller/PurchaseController.java
@@ -1,12 +1,32 @@
 package org.example.bootlog.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//import java.util.logging.Logger;
+
+
 @RestController
 public class PurchaseController {
+
+//    private final Logger logger = Logger.getLogger(PurchaseController.class.getName());
+    private final Logger logger = LoggerFactory.getLogger(PurchaseController.class);
+
     @PostMapping
     public void purchase() {
         System.out.println("purchase");
+        logger.info("purchase");
+        // verbose - debug - info - warn - error - critical
+        // finest - fine - info - warning - severe
+        try {
+            throw new RuntimeException("purchase error");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+//            logger.severe(e.getMessage());
+            logger.error(e.getMessage());
+        }
+
     }
 }
